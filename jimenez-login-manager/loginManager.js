ac9cd6effd9fa5763e0fce48a514f495f5e23ce2
jonas <jonasnahum@gmail.com>
2015-03-31 18:04:39 -0600

    database authentication

--- jimenez-login-manager/loginManager.js
+++ jimenez-login-manager/loginManager.js
@@ -1,18 +1,52 @@
 exports.init = function () {
+    var couchdb= require("jimenez-couchdb-endpoints");
+    var db = new couchdb.Database("contribuyentes_users");
+    
     "use strict";
     var LoginManager = function () {
     };
     LoginManager.prototype.verifyCredentials = function (userName, password, done) {
-        if (userName === password) {
-            done(null, {id: userName, name: userName});
-        } else {
-            done(null, null);
-        }
+        
+        var query = new couchdb.Query("contribuyentes_users", "contribuyentes", "byusername");
+        var endPoint = query.getEndPoint({
+            startkey: encodeURIComponent('"' + userName + '"'),
+            endkey: encodeURIComponent('"' + userName + '"'),
+        });
+        
+        var factory = couchdb.PromiseFactory();
+        var promise = factory.getPromise(endPoint);
+        
+        promise.then(function(args){
+            var body = JSON.parse(args[1]);
+            var doc = null;
+            var success = false;
+            
+            if (body.rows.length){
+                doc = body.rows[0];
+                if (doc.username === userName && doc.password === password){
+                    success = true;
+                }
+            }
+            
+            if (success) {
+                done(null, doc);
+            } else {
+                done(null, null);
+            }                
+        });
+        
+        promise.fail(function(err, couchRes, body){
+            done(err, null);
+        });
+        
+        
     };
     LoginManager.prototype.serializeUser = function (user, done) {
         done(null, user.id);
     };
     LoginManager.prototype.deserializeUser = function (id, done) {
+        var endpointView = db.view(id);
+        
         done(null, {id: id, name: id});
     };
                                            
